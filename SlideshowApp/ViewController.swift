8742d0939a6bc7763157bb23f0ecc964558fa125
katsuhiko.harada <h-katsuhiko@alto.ocn.ne.jp>
2018-04-13 07:29:58 +0900

    SlideShowAppのレベルを追加した。

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -16,24 +16,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
     
+    @IBOutlet weak var startstopButton: UIButton!
     
     @IBOutlet weak var imageView: UIImageView!
     
 
     @IBAction func onTapimage(_ sender: Any) {
+        if self.timer == nil {
+        }else{
+            self.timer.invalidate()
+            self.timer = nil
+            backButton.isEnabled = true
+            nextButton.isEnabled = true
+            startstopButton.setTitle("再生", for: .normal)
+        }
         performSegue(withIdentifier:"result", sender: nil)
     }
     @IBAction func onPrev(_ sender: Any) {
         dispImageNo -= 1
-        displayImage()
+        displayImage()//画像を表示する処理
     }
     @IBAction func onNext(_ sender: Any) {
         dispImageNo += 1
-        displayImage()
+        displayImage()//画像を表示する処理
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         //次の画面に画像のフィアイル名を引き渡す
-        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController//画像の位置を表している。
         let imageNameArray = [
             "IMG_0065.jpg",
             "IMG_0061.jpg",
@@ -41,24 +50,27 @@ class ViewController: UIViewController {
             "IMG_0063.jpg",
             "IMG_0069.jpg",
             ]
-        let name = imageNameArray[dispImageNo]
+        let name = imageNameArray[dispImageNo]//現在imagenameに表示しているファイル名 を表している。
         
       resultViewController.imageName = name//"IMG_0065.jpg"//今表示している画像の情報
+        
+        
+        
     }
     @IBAction func startstop(_ sender: Any) {
-  //     if runnig{
-     
-       
-//      backButton.setTitle(<#T##title: String?##String?#>, for: <#T##UIControlState#>)
-//       }
+
         if self.timer == nil {
-         backButton.isEnabled = false
-         nextButton.isEnabled = false
+            backButton.isEnabled = false
+            nextButton.isEnabled = false
+            startstopButton.setTitle("停止", for: .normal)
+            
             self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         }
         else {
             backButton.isEnabled = true
             nextButton.isEnabled = true
+            startstopButton.setTitle("再生", for: .normal)
+            
             self.timer.invalidate()
             self.timer = nil
         }
@@ -86,8 +98,6 @@ class ViewController: UIViewController {
         let image = UIImage(named: name)
         imageView.image = image
     }
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
